1d2abc8145d7bdead6cee56748aac80d64d93b61
Jimit G <jimitkg@gmail.com>
2021-06-21 19:51:23 +1000

    Comments added descibing what quires are used for

--- query msdb mail items.sql
+++ query msdb mail items.sql
@@ -1,4 +1,9 @@
+
+-- get emails log from msdb database
+
 select top 100 * from msdb..sysmail_allitems
 order by sent_date desc
+
+--fetch registered mail profiles 
 
 select * from msdb..sysmail_account

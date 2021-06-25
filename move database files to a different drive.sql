7dca0d86ccebb8957de40c75a87411bb335d8979
Jimit G <jimitkg@gmail.com>
2021-06-25 21:09:01 +1000

    create file

---
+++ move database files to a different drive.sql
@@ -0,0 +1,28 @@
+-- mark database offline 
+use master
+go
+
+ALTER DATABASE dbname set offline
+ with rollback immediate
+
+--get file details
+sp_helpdb dbname
+
+--note details and copy for reference
+
+--move database files (copy and paste)
+--change logical pointer on master database
+
+USE master;
+ALTER DATABASE dbname
+MODIFY FILE (name='dbname'
+             ,filename='E:\db.mdf')
+
+			 ALTER DATABASE epo
+MODIFY FILE (name='epo_log'
+             ,filename='E:\db_log.LDF')
+
+
+--turn database back online 
+ALTER DATABASE dbname set online
+ with rollback immediate
